3f814d539433a6f9b779ccf26b06f86760016b3e
stevenvanophem <stevenvanophem@gmail.com>
2023-09-22 01:27:17 +0200

    Add tests

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -3,9 +3,16 @@ package com.gildedrose;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.ArgumentsSource;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -22,6 +29,36 @@ class GildedRoseTest {
     @DisplayName("I can add items to the GildedRose")
     void testAddItems() {
         provideItems().forEach(app::addItem);
+    }
+
+    @ParameterizedTest(name = "Day {1} expected sellIn: {2} and quality: {3}")
+    @MethodSource
+    @DisplayName("Once the sell by date has passed, Quality degrades twice as fast")
+    void testRule1(TestItem testItem, int days, int sellIn, int quality) {
+        final Item item = testItem.toItem();
+
+        app.addItem(item);
+        IntStream.range(0, days).forEach(day -> app.updateQuality());
+
+        assertThat(item.toString()).isEqualTo(item.name + ", " + sellIn + ", " + quality);
+    }
+
+    private static Stream<Arguments> testRule1() {
+        final TestItem cookies = new TestItem("Cookies", 5, 10);
+        return Stream.of(
+            Arguments.of(cookies, 0, 5, 10),
+            Arguments.of(cookies, 1, 4, 9),
+            Arguments.of(cookies, 2, 3, 8),
+            Arguments.of(cookies, 3, 2, 7),
+            Arguments.of(cookies, 4, 1, 6),
+            Arguments.of(cookies, 5, 0, 5),
+            Arguments.of(cookies, 6, -1, 3),
+            Arguments.of(cookies, 7, -2, 1),
+            Arguments.of(cookies, 8, -3, 0),
+            Arguments.of(cookies, 9, -4, 0),
+            Arguments.of(cookies, 10, -5, 0),
+            Arguments.of(cookies, 11, -6, 0)
+        );
     }
 
     @Test
@@ -93,6 +130,14 @@ class GildedRoseTest {
             // this conjured item does not work properly yet
             new Item("Conjured Mana Cake", 3, 6)
         );
+    }
+
+    record TestItem(String name, int sellIn, int quality) {
+
+        public Item toItem() {
+            return new Item(name, sellIn, quality);
+        }
+
     }
 
 }
